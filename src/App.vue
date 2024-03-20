c0a065cfe95ec4a22c580a50b62145d649f21cdc
kurokoo <80524293+lassksy@users.noreply.github.com>
2024-03-20 20:21:36 +0800

    更新样式

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,11 @@
 </script>
 
 <template>
-    <div class="navigate">
-      <RouterLink to="/home">首页</RouterLink>
-      <RouterLink to="/top">排行榜</RouterLink>
-      <RouterLink to="/playlist">歌单</RouterLink>
+    <div class="main">
+      <div class="navigate">
+      <RouterLink class="router-link" to="/home">首页</RouterLink>
+      <RouterLink class="router-link" to="/top">排行榜</RouterLink>
+      <RouterLink class="router-link" to="/playlist">歌单</RouterLink>
 
 
     </div>
@@ -14,8 +15,40 @@
     <div class="main-content">
         <RouterView></RouterView>
     </div>
+    </div>
+    
 </template>
 
 <style scoped>
+      
+      .navigate {
+        position: fixed; /* 固定在屏幕上 */
+        left: 0;
+        top: 0;
+        bottom: 0; /* 填充整个屏幕 */
+        width: 50px; /* 侧边栏宽度 */
+        background-color: aliceblue; /* 侧边栏背景色 */
+        padding: 20px; 
+        }
+
+      .router-link {
+        display: block;
+        text-decoration: none;
+        margin-top: 20px;
+        font-weight: 700;
+        text-align: center;
+      }
+
+      .router-link-active  {
+        background-color: hsl(0, 0%, 0%);
+        color: rgb(255, 255, 255);
+        transition: 0.4s;
+        border-radius: 0.25rem;
+        cursor: pointer;
+      }
+
+      .main-content {
+        padding-left: 100px;
+      }
 
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,7 @@
 <template>
-    
+    <div class="header">
+        <div class="search-box">搜索</div>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -8,6 +10,13 @@
 
 
 <style scoped>
+    .hader {
+        background-color: aliceblue;
+    } 
 
+    .search-box  {
+        border-radius: 25px;
+        background-color: white;
+    }
 
 </style>
